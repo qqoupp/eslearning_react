0d345694dd7c91fbd2baf56543a16bd7eb64bca0
qqoupp <74557790+qqoupp@users.noreply.github.com>
2023-11-20 23:47:24 +0000

    created signIn and signUp pages

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const SignIn = () => {
+    return (
+        <>
+            <h1
+                className='text-4xl text-center text-blue-500 mt-10'
+            >Sign In</h1>
+        </>
+    );
+}
+export default SignIn;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const SignUp = () => {
+    return (
+        <>
+            <h1
+                className='text-4xl text-center text-blue-500 mt-10'
+            >Sign Up</h1>
+        </>
+    );
+}
+export default SignUp;
