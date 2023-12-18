34efe8690cc7b83be592c2c8d103f28ddb1efc98
qqoupp <74557790+qqoupp@users.noreply.github.com>
2023-12-18 11:23:26 +0000

    finished the user provider functionality

--- src/components/NavigationDrawer/index.tsx
+++ src/components/NavigationDrawer/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Drawer, List, ListItem, Paper } from "@mui/material";
 import { ClickAwayListener } from "@mui/base/ClickAwayListener";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../providers/userProvider";
 
 type NavigationDrawerProps = {
   open: boolean;
   setOpen: (open: boolean) => void;
 };
 
-const NavigationDrawer: React.FC<NavigationDrawerProps> = ({ open, setOpen }) => {
+const NavigationDrawer: React.FC<NavigationDrawerProps> = ({
+  open,
+  setOpen,
+}) => {
   const navigate = useNavigate();
+
+  const { isLoggedIn } = React.useContext(UserContext);
 
   const handleNavigate = (path: string) => {
     setOpen(false);
@@ -25,51 +31,61 @@ const NavigationDrawer: React.FC<NavigationDrawerProps> = ({ open, setOpen }) =>
       >
         <Paper
           style={{
-            background:'#001524',
-            color: 'white', 
+            background: "#001524",
+            color: "white",
           }}
         >
           <div className="px-5 py-7 min-h-screen">
-          <div className="flex flex-col justify-between gap-y-5">
-                <div className="flex flex-col pb-40 gap-0">
-                  <span className="font-bold text-xl text-white">
-                    Elevate
-                  </span>
-                  <span className="font-bold text-xl text-white">
-                    StackLearning
-                  </span>
-                </div>
-                <div className="flex flex-col align-items-centre">
-                  <List>
-                  <ListItem button className="text-xl gap-y-5">
-                      Project Guide
-                    </ListItem>
-                    <ListItem button className="text-xl gap-y-5">
-                      Project Ideea
-                    </ListItem>
-                    <ListItem
-                      button
-                      className="text-xl gap-y-5"
-                      onClick={() => handleNavigate("/signin")}
-                    >
-                      Sign In
-                    </ListItem>
-                    <ListItem
-                      button
-                      className="text-xl gap-y-5"
-                      onClick={() => handleNavigate("/signup")}
-                    >
-                      Sign Up
-                    </ListItem>
-                    <ListItem button className="text-xl gap-y-5">
-                      About
-                    </ListItem>
-                    <ListItem button className="text-xl gap-y-5">
-                      Contact
-                    </ListItem>
-                  </List>
-                </div>
+            <div className="flex flex-col justify-between gap-y-5">
+              <div className="flex flex-col pb-40 gap-0">
+                <span className="font-bold text-xl text-white">Elevate</span>
+                <span className="font-bold text-xl text-white">
+                  StackLearning
+                </span>
               </div>
+              <div className="flex flex-col align-items-centre">
+                <List>
+                  {isLoggedIn ? (
+                    <div>
+                      <ListItem
+                        button
+                        className="text-xl gap-y-5"
+                        onClick={() => handleNavigate("/guide")}
+                      >
+                        Project Guide
+                      </ListItem>
+                      <ListItem button className="text-xl gap-y-5">
+                        Project Ideea
+                      </ListItem>
+                    </div>
+                  ):(
+                    <div>
+                      <ListItem
+                        button
+                        className="text-xl gap-y-5"
+                        onClick={() => handleNavigate("/signin")}
+                      >
+                        Sign In
+                      </ListItem>
+                      <ListItem
+                        button
+                        className="text-xl gap-y-5"
+                        onClick={() => handleNavigate("/signup")}
+                      >
+                        Sign Up
+                      </ListItem>
+                    </div>
+                  )
+                  }
+                  <ListItem button className="text-xl gap-y-5">
+                    About
+                  </ListItem>
+                  <ListItem button className="text-xl gap-y-5">
+                    Contact
+                  </ListItem>
+                </List>
+              </div>
+            </div>
           </div>
         </Paper>
       </ClickAwayListener>
